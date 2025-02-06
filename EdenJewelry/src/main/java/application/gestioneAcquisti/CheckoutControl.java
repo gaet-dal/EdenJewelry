627d7c27583c06dfd40ea914bf13f382c26299b7
miria <m.desantis40@studenti.unisa.it>
2025-02-06 11:33:59 +0100

    fix completo CheckoutControl

--- EdenJewelry/src/main/java/application/gestioneAcquisti/CheckoutControl.java
+++ EdenJewelry/src/main/java/application/gestioneAcquisti/CheckoutControl.java
@@ -5,7 +5,10 @@ import main.java.dataManagement.bean.RigaOrdineBean;
 import main.java.dataManagement.dao.OrdineDAO;
 import main.java.dataManagement.bean.ProdottoBean;
 import main.java.dataManagement.dao.ProdottoDAO;
+import main.java.dataManagement.dao.RigaOrdineDAO;
+import main.java.dataManagement.dao.UtenteDAO;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
@@ -52,13 +55,27 @@ public class CheckoutControl {
         }
     }
 
-    public static boolean checkout(Carrello carrello, String metodoPagamento, String indirizzo, String email, OrdineDAO dao, ProdottoDAO prodotti){
+    public static boolean checkout(Carrello carrello, String email, String metodoPagamento, float totale, String indirizzo, DataSource ds){
         // I dati non sono validi, non si può proseguire con il checkout
         if (!checkIndirizzo(indirizzo) || checkMetodoPagamento(metodoPagamento) || carrello.isEmpty())
             return false;
 
+        OrdineDAO ordini=new OrdineDAO(ds);
+        ProdottoDAO prodotti =new ProdottoDAO(ds);
+        RigaOrdineDAO righe =  new RigaOrdineDAO(ds);
         OrdineBean ordine = new OrdineBean();
-        float totale = 0.0f;
+        totale = 0.0f;
+
+        //recuperiamo l'ultimo ordine effettuato;
+        ordine= ordini.doRetrieveUltimoOrdine();
+
+        int numeroOrdine=0;
+        if(ordine!=null){
+
+            numeroOrdine=ordine.getIdOrdine();
+            numeroOrdine+=1; //creiamo il l'indentificativo dell'oridne che dovvrà essere effettuato e salvato nel db;
+        }
+
 
         List<ItemCarrello> list = carrello.getListProdotti();
         ItemCarrello []arr= list.toArray(new ItemCarrello[0]);
@@ -66,15 +83,25 @@ public class CheckoutControl {
         for(ItemCarrello it : arr) {
             String nome = it.getNome();
             ProdottoBean p;
+
             RigaOrdineBean riga = new RigaOrdineBean();
 
             try{
                 p = prodotti.doRetrieveByNome(nome);
             } catch (SQLException e) {
+                logger.warning("Errore nel fetch del prodotto");
                 throw new RuntimeException(e);
             }
 
+            riga.setNumeroOrdine(numeroOrdine);
+            riga.setNomeProdotto(nome);
+            riga.setQuantità(it.getQuantità());
+
+
             totale += p.getPrezzo() * it.getQuantità();
+            riga.setPrezzoUnitario(p.getPrezzo());
+            righe.doSave(riga); //salviamo la riga nel db;
+
         }
 
         //salvataggio dei dati nel bean per la memorizazzione
@@ -86,7 +113,7 @@ public class CheckoutControl {
         boolean result = false;
 
         try {
-            result = dao.doSave(ordine);
+            result = ordini.doSave(ordine);
         } catch (SQLException e) {
             logger.warning("Errore nel salvataggio dell'ordine");
             e.printStackTrace();
